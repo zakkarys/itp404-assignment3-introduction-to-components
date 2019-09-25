b87554d2eaa1c6ec19b3c7d02803fd67219ea8de
Zakkary Smith <zakkarys@usc.edu>
2019-09-24 23:25:17 -0700

    fixed

--- react-class/src/App.js
+++ react-class/src/App.js
@@ -45,7 +45,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        
+        {this.state.IncrementItem}
         <PreviousSearches previousSearches={this.state.previousSearches} applyPreviousSearch={this.applyPreviousSearch}/>
         <SearchForm onSearch={this.handleSearch} />
         <div onClick={this.IncrementItem} >
@@ -58,37 +58,3 @@ class App extends React.Component {
 
 export default App;
 
-////
-/// class App extends React.Component {
-
-//   constructor(){
-//     super();
-//     this.state = {
-//       results:[],
-//       loading:true
-//     };
-//   }
-
-
-//   async componentDidMount() {
-    
-//     let results = await getReddits('design');
-
-//     this.setState({ 
-//      results, loading: false
-//     });
-//   }
-//   render() {
-//     return (
-//         <div>
-//         <h1>{this.state.subscribers}</h1>
-//           <div>
-//          {this.state.loading ? <Loading /> : this.state.results.data.children.map((result) => {
-//             return <RedditCard result={result} key={result.data.id} />
-//           })}
-//           </div>
-//         </div>
-//     );
-//   }
-// }
-// export default App;
